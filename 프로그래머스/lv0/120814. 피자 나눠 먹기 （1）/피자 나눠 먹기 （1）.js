adadeaee9fb16a37d1de874ed61a63467476fbd0
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-03-21 22:00:55 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    if (n % 7 === 0) {
+        return Math.floor(n / 7);
+    } else if (n % 7 === 7) {
+        return 1;
+    } else if (n % 7 < 7) {
+        return Math.floor(n / 7 + 1);
+    }
+}
