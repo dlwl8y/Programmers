3401cd3a90a3062a6049bdecd3f51a99040ce319
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-03-22 21:48:44 +0900

    Update 피자 나눠 먹기 （2）.js

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -7,3 +7,11 @@ function solution(n) {
         max++;
     }
 }
+
+// function solution(n) {
+//     let pizza = 1;
+//     while (pizza * 6 % n) {
+//         pizza++;
+//     }
+//     return pizza;
+// }
