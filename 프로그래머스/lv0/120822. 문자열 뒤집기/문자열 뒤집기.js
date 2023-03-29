2a00e0eaaeecb877f167bf4de12fe54faf9355df
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-03-30 00:03:21 +0900

    Update 문자열 뒤집기.js

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -6,3 +6,12 @@ function solution(my_string) {
     }
     return result;
 }
+
+// function solution(my_string) {
+//     return my_string.split('').reverse().join('');
+// }
+
+// function solution(my_string) {
+//     var answer = [...my_string].reverse().join("");
+//     return answer;
+// }
