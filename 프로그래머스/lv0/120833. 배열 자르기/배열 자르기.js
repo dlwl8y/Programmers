2899df95062f3dea2843f3033bc433620a6a03ad
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-03 22:36:34 +0900

    Update 배열 자르기.js

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -7,3 +7,13 @@ function solution(numbers, num1, num2) {
     
     return newArr;
 }
+
+
+// function solution(numbers, num1, num2) {
+//     return numbers.slice(num1, num2 + 1);
+// }
+
+
+// function solution(numbers, num1, num2) {
+//     return numbers.filter((n, i) => num1 <= i && i <= num2);
+// }
