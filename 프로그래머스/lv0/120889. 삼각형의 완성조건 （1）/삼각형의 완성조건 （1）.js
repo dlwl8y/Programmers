9b5b3cb31fa859305c07c1cb31da9877bb252299
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-10 00:24:43 +0900

    Update 삼각형의 완성조건 （1）.js

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -2,3 +2,14 @@ function solution(sides) {
     const result = sides.sort((a,b) => a-b);
     return result[0]+result[1] > result[2] ? 1 : 2;
 }
+
+
+// function solution(sides) {
+//     ler result = 0;
+//     const max = Math.max(...sides);
+//     const sum = sides.reduce((a,b) => a + b, 0) - max;
+
+//     result = max < sum? 1 : 2;
+
+//     return result;
+// }
