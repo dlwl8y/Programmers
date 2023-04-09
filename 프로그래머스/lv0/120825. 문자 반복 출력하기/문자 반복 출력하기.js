7e6477201c854078a8b22a08590ad0717f7bb890
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-10 01:11:35 +0900

    Update 문자 반복 출력하기.js

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -2,3 +2,15 @@ function solution(my_string, n) {
     const result = [...my_string].map(el => el.repeat(n)).join('')
     return result;
 }
+
+
+// function solution(my_string, n) {
+//     return my_string.split('').reduce((acc, cur) => acc + cur.repeat(n), '')
+// }
+
+
+// function solution(my_string, n) {
+//     let result = '';
+//     for (let i of my_string) result+=i.repeat(n);
+//     return result;
+// }
