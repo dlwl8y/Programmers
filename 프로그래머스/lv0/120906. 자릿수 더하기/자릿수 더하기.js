034cf25fdf1da52b40630c2f293fac860b9aea98
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-14 00:15:38 +0900

    Update 자릿수 더하기.js

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -7,3 +7,11 @@ function solution(n) {
     }
     return sum;
 }
+
+
+// function solution(n) {
+//   return n
+//     .toString()
+//     .split("")
+//     .reduce((acc, cur) => acc + Number(cur), 0);
+// }
