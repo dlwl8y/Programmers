4f943b677fba6a6b826f4e717cce822c322a13f5
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-24 23:04:25 +0900

    Update 문자열 정렬하기 （1）.js

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -2,3 +2,13 @@ function solution(my_string) {
     const result = my_string.split('').map(Number).filter(el => !isNaN(el)).sort((a,b) => a-b);
     return result;
 }
+
+
+// function solution(my_string) {
+//     return my_string.match(/\d/g).sort((a, b) => a - b).map(n => Number(n));
+// }
+
+
+// function solution(my_string) {
+//     return my_string.replace(/[^0-9]+/g, '').split('').map(val => +val).sort((a, b) => a - b);
+// }
