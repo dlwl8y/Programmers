1fd66db31936ee1ceaa1e7da4c7532e6a6684da7
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-27 23:19:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,15 @@
+function solution(my_string, num1, num2) {
+    let mem = my_string[num1];
+    let result = '';
+    for (let i = 0; i < my_string.length; i++) {
+        if (i === num1) {
+            result += my_string[num2];
+        } else if (i === num2) {
+            result += my_string[num1];
+        } else {
+            result += my_string[i];
+        }
+    }
+    return result;
+    
+}
