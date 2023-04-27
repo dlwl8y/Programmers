2cb7a3d8e9dd7371918e0a81fa769a564912c94c
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-04-27 23:21:07 +0900

    Update 인덱스 바꾸기.js

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -13,3 +13,10 @@ function solution(my_string, num1, num2) {
     return result;
     
 }
+
+
+// function solution(my_string, num1, num2) {
+//   let arr = my_string.split("");
+//   [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+//   return arr.join("");
+// }
