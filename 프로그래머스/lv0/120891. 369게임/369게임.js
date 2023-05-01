928f441403a305bf0afc93aca095eafe855b88f8
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-05-02 00:24:21 +0900

    Update 369게임.js

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -2,3 +2,9 @@ function solution(order) {
     let result = [...String(order)].map((el) => Number(el));
     return result.reduce((a, c) => c % 3 === 0 && c !== 0 ? a += 1 : a += 0, 0);
 }
+
+
+// function solution(order) {
+//     var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+//     return answer;
+// }
