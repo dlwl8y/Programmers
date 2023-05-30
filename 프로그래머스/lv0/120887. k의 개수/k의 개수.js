96751fa05d332c3f4d010a06dcd31accabc570bc
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-05-31 02:11:53 +0900

    Update k의 개수.js

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -7,3 +7,14 @@ function solution(i, j, k) {
 
     return result.split(k).length - 1;
 }
+
+
+// function solution(i, j, k) {
+//     let str = Array(j - i + 1).fill(i).map((v, i) => v + i).join('')
+//     return Array.from(str).filter(t => +t === k).length;
+// }
+
+
+// function solution(i, j, k) {
+//     return Array(j-i+1).fill(i).map((v,i)=>v+i).join('').split(k).length-1;
+// }
