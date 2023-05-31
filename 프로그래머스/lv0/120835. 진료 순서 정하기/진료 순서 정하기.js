c7477d3e576aea9f550cc97ca5fb90d6f5f5d5a7
dlwl8y <91511828+dlwl8y@users.noreply.github.com>
2023-06-01 00:17:32 +0900

    Update 진료 순서 정하기.js

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,5 @@
 function solution(emergency) {
+    // [...emergency] 대신 emergency.slice() 도 가능
     const sorted = [...emergency].sort((a, b) => b - a);
     return emergency.map(v => sorted.indexOf(v) + 1);
 }
